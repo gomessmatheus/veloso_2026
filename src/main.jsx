c829a4ac4c5df3a0ed337b2f0562a24b232415c1
gomessmatheus <matheussgbf@gmail.com>
2026-05-05 13:54:36 -0300

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
+
+// Show any error before React mounts
+window.onerror = (msg, src, line, col, err) => {
+  document.getElementById('root').innerHTML = 
+    `<div style="padding:40px;font-family:monospace;background:#FFF1F2;color:#A32D2D;border:2px solid #FCA5A5;border-radius:8px;margin:20px">
+      <b>ERRO</b><br/>${msg}<br/>Linha: ${line}<br/>${src}
+    </div>`;
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <React.StrictMode><App/></React.StrictMode>
 )

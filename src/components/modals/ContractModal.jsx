220f79ef9a564e90ba0de7397636995b2fb80981
gomessmatheus <matheussgbf@gmail.com>
2026-05-09 13:36:00 -0300

    chore(scaffold): src/components/modals/ContractModal.jsx placeholder

---
+++ src/components/modals/ContractModal.jsx
@@ -0,0 +1,3 @@
+// src/components/modals/ContractModal.jsx
+// TODO: mover ContractModal de App.jsx. Inclui validações: datas válidas, valores >= 0,
+// soma de installments deve casar com contractValue (warning não-bloqueante).

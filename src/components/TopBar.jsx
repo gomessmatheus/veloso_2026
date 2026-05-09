121df88c05edeb4371c381b5352078e6e8d27086
gomessmatheus <matheussgbf@gmail.com>
2026-05-09 13:33:10 -0300

    chore(scaffold): src/components/TopBar.jsx placeholder

---
+++ src/components/TopBar.jsx
@@ -0,0 +1,3 @@
+// src/components/TopBar.jsx
+// TODO: mover TopBar de App.jsx. Mostrar syncStatus (dot colorido + tooltip), presence avatars,
+// inputs de eurRate/usdRate, botões 'Novo contrato/post/tarefa' por papel.

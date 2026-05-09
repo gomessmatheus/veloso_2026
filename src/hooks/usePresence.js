a6d837ccc1d507c6e937da2098ec1532920e18f4
gomessmatheus <matheussgbf@gmail.com>
2026-05-09 13:28:04 -0300

    chore(scaffold): src/hooks/usePresence.js placeholder

---
+++ src/hooks/usePresence.js
@@ -0,0 +1,4 @@
+// src/hooks/usePresence.js
+// TODO: chamar updatePresence em interval (heartbeat ~30s) + subscribeToPresence.
+// Cleanup: removePresence no unmount/beforeunload.
+// Usar displayName/email do auth quando disponível.

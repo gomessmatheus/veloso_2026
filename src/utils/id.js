e5b071145ac7d62a200c0956b9888ee9b6790419
gomessmatheus <matheussgbf@gmail.com>
2026-05-09 13:23:50 -0300

    chore(scaffold): src/utils/id.js placeholder

---
+++ src/utils/id.js
@@ -0,0 +1,3 @@
+// src/utils/id.js
+// TODO: implementar uid() usando crypto.randomUUID() com fallback para Math.random.
+// Substitui o uid antigo de App.jsx (Math.random().toString(36).substr(2,8)).

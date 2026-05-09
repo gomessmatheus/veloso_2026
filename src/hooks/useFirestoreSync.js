8e251952c10f3ecbfb8c3ff95a6582aa55073d01
gomessmatheus <matheussgbf@gmail.com>
2026-05-09 13:28:47 -0300

    chore(scaffold): src/hooks/useFirestoreSync.js placeholder

---
+++ src/hooks/useFirestoreSync.js
@@ -0,0 +1,3 @@
+// src/hooks/useFirestoreSync.js
+// TODO: usar subscribeToChanges + manter estados contracts/posts/deliverables/settings.
+// Expor syncStatus: 'syncing' | 'online' | 'offline' | 'error'.
